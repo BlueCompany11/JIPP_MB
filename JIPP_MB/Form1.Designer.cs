5279f592575c3ceae8aa69ebf2f17e72077dbdf9
BlueCompany <gronczewskim@gmail.com>
2018-08-22 21:40:13 +0200

    a

--- JIPP_MB/Form1.Designer.cs
+++ JIPP_MB/Form1.Designer.cs
@@ -29,12 +29,126 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            this.studentsDbDataSet = new JIPP_MB.StudentsDbDataSet();
+            this.studentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.studentsTableAdapter = new JIPP_MB.StudentsDbDataSetTableAdapters.StudentsTableAdapter();
+            this.forenameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.indexDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBoxAnimation = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsDbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAnimation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridViewStudents, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxAnimation, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(692, 429);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.AllowUserToOrderColumns = true;
+            this.dataGridViewStudents.AutoGenerateColumns = false;
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.forenameDataGridViewTextBoxColumn,
+            this.surnameDataGridViewTextBoxColumn,
+            this.indexDataGridViewTextBoxColumn});
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridViewStudents, 2);
+            this.dataGridViewStudents.DataSource = this.studentsBindingSource;
+            this.dataGridViewStudents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(10, 10);
+            this.dataGridViewStudents.Margin = new System.Windows.Forms.Padding(10);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.Size = new System.Drawing.Size(672, 194);
+            this.dataGridViewStudents.TabIndex = 0;
+            this.dataGridViewStudents.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridViewStudents_KeyDown);
+            // 
+            // studentsDbDataSet
+            // 
+            this.studentsDbDataSet.DataSetName = "StudentsDbDataSet";
+            this.studentsDbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentsBindingSource
+            // 
+            this.studentsBindingSource.DataMember = "Students";
+            this.studentsBindingSource.DataSource = this.studentsDbDataSet;
+            // 
+            // studentsTableAdapter
+            // 
+            this.studentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // forenameDataGridViewTextBoxColumn
+            // 
+            this.forenameDataGridViewTextBoxColumn.DataPropertyName = "Forename";
+            this.forenameDataGridViewTextBoxColumn.HeaderText = "Forename";
+            this.forenameDataGridViewTextBoxColumn.Name = "forenameDataGridViewTextBoxColumn";
+            // 
+            // surnameDataGridViewTextBoxColumn
+            // 
+            this.surnameDataGridViewTextBoxColumn.DataPropertyName = "Surname";
+            this.surnameDataGridViewTextBoxColumn.HeaderText = "Surname";
+            this.surnameDataGridViewTextBoxColumn.Name = "surnameDataGridViewTextBoxColumn";
+            // 
+            // indexDataGridViewTextBoxColumn
+            // 
+            this.indexDataGridViewTextBoxColumn.DataPropertyName = "Index";
+            this.indexDataGridViewTextBoxColumn.HeaderText = "Index";
+            this.indexDataGridViewTextBoxColumn.Name = "indexDataGridViewTextBoxColumn";
+            // 
+            // pictureBoxAnimation
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBoxAnimation, 2);
+            this.pictureBoxAnimation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxAnimation.Location = new System.Drawing.Point(3, 217);
+            this.pictureBoxAnimation.Name = "pictureBoxAnimation";
+            this.pictureBoxAnimation.Size = new System.Drawing.Size(686, 209);
+            this.pictureBoxAnimation.TabIndex = 1;
+            this.pictureBoxAnimation.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(692, 429);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsDbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAnimation)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
+
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+        private StudentsDbDataSet studentsDbDataSet;
+        private System.Windows.Forms.BindingSource studentsBindingSource;
+        private StudentsDbDataSetTableAdapters.StudentsTableAdapter studentsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn forenameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn indexDataGridViewTextBoxColumn;
+        private System.Windows.Forms.PictureBox pictureBoxAnimation;
     }
 }
 
--- JIPP_MB/Form1.cs
+++ JIPP_MB/Form1.cs
@@ -12,9 +12,118 @@ namespace JIPP_MB
 {
     public partial class Form1 : Form
     {
+        public delegate void SaveToDb();
+        public event SaveToDb DbUpdateFinished;
+        delegate void TimerStop();
+        event TimerStop TimerStopHandler;
+        int ticks;
+        System.Timers.Timer timer;
+        bool result;
+        int dx = 5;
+        int dy = 50;
         public Form1()
         {
             InitializeComponent();
+            //dodaje do eventu metode do wykonania
+            DbUpdateFinished += Form1_DbUpdateFinished;
+            //to samo co wyzej tylko zamiast deklarowac ta metode korzystam z wyrazenia lambda
+            //to () =>{} oznacza, ze dopisuje do TimerStopHandler metode ktora przyjmuje 0 zmiennych i nic nie zwraca czyli zgodnie
+            //z delegata TimerStop ktora jest void i nic nie przyjmuje
+            TimerStopHandler += (() =>
+            {   //metoda ta resetuje ustawienia 
+                ticks = 0;
+                timer.Stop();
+                dx = 5;
+                dy = 50;
+            });
+            //ustawiam timer
+            timer = new System.Timers.Timer(200);
+            timer.AutoReset = true;
+            //dodaje do timera metode ktora sie bedzie wykonywac za kazdym ticknieciem
+            timer.Elapsed += Timer_Elapsed;
+            //poki co timer jeszcze nie ejst uruchominoy
         }
+
+        private void Form1_DbUpdateFinished()
+        {
+            //metoda ktora jest przypisana do eventu DbUpdateFinished
+            //gdy sie powiedzie modyfikwacja bazy danych to kolor kontrolki na dole zmieni sie na odpowiedni
+            if (result == true)
+                pictureBoxAnimation.BackColor = Color.AliceBlue;
+            else
+                pictureBoxAnimation.BackColor = Color.Bisque;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //zadaniem metody dopisanej do timera jest wywolywanie metody ktora rysuje
+            DrawAnimation();
+            ticks++;
+            if (ticks > 5)
+            {
+                //wywoluje zdarzenie TimerStopHandler gdy ilosc tickniec jest wieksza od 5
+                //czyli wywola sie ta lambda ktora jest w konstuktorze Form1
+                //czyli zresetuje wartosci i zatrzyma timer
+                TimerStopHandler();
+            }
+            
+        }
+        private void DrawAnimation()
+        {
+            //rysowanie animacji - nie ma co tu tlumaczyc kod sztampowy z oficjalnej strony microsoft
+            //podrasowany by spelnial moje zalozenia
+            Bitmap bmp1 = new Bitmap(pictureBoxAnimation.Width, pictureBoxAnimation.Height);
+            Graphics graphics = Graphics.FromImage(bmp1);
+            string textToShow = "Zmiana poprawna";
+            if (result == false)
+            {
+                textToShow = "Zmiana niepoprawna";
+            }
+            //przesuwanie napisu o dx poziomo i dy w pionie
+            graphics.TranslateTransform(dx, dy);
+            //dzialanie modulo bo moglo wyjsc poza ekran i by nie bylo widac zmian, a tak to zacznie od poczatku jak wyjdzie poza kontrolke
+            dx = (dx + 70) % pictureBoxAnimation.Width;
+            //raz do gory raz do dolu
+            dy *= (-1);
+            System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 16);
+            System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+            System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
+            graphics.DrawString(textToShow, drawFont, drawBrush, 10, 100, drawFormat);
+            pictureBoxAnimation.BackgroundImage = bmp1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'studentsDbDataSet.Students' table. You can move, or remove it, as needed.
+            this.studentsTableAdapter.Fill(this.studentsDbDataSet.Students);
+        }
+
+        private void dataGridViewStudents_KeyDown(object sender, KeyEventArgs e)
+        {
+            //jesli przyciskiem ktory spowodowal zdarzenie keydown jest enter to:
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    //update bazy - zapisze sie to co sie znajduje w tej kontrolce w trakcie dzialania programu
+                    studentsTableAdapter.Update(studentsDbDataSet.Students);
+                    result = true;
+                    //wywolanie zdarzenia 
+                    DbUpdateFinished();
+                    
+                    //uruchamiamy timera
+                    timer.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    //w to praktycznie nigdy nie wejdziemy bo kontrolka sama zabezpiecza nas przed wprowadzeniem zlych danych
+                    result = false;
+                    DbUpdateFinished();
+                    timer.Enabled = true;
+                }
+                
+            }
+        }
+
     }
 }
